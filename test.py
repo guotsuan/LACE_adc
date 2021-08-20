7d95ea6c95585ffa15387e610b732b33e3c8d588
Quan Guo <guotsuan@gmail.com>
2021-08-20 23:56:02 +0800

    New way of copy data from buffer the excution time from 1.7 to 0.0004, speed up 4000 times

--- test.py
+++ test.py
@@ -22,35 +22,51 @@ def id_offset(t0, t1, cycle):
 src_udp_ip = "192.168.90.20"
 src_udp_port = 59000
 
-
 udp_ip = "192.168.90.100"
 udp_port = 60000
 
+cycle = 49999
+
 sock = socket.socket(socket.AF_INET,  socket.SOCK_RAW, socket.IPPROTO_UDP)
 err = sock.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, 1073741824)
-
-# sock.setsockopt(socket.SO_NO_CHECK,1)
-
+# err = sock.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, 262144*8) 
 
 sock.bind((udp_ip, udp_port))
 
-# sock.connect((src_udp_ip, src_udp_port))
-# data, addr=sock.recvfrom(8000)
-
-pyload_size = 8192
-header_size = 28
 count = 20000
 
-packet_size = pyload_size + header_size
-udp_p = bytearray(count*packet_size)
-mem_buff = memoryview(udp_p)
+payload_size = 8192
+header_size = 28
+header_id_size = 2
+packet_size = payload_size + header_size
+
+udp_packet = bytearray(count*packet_size)
+udp_payload = bytearray(count*(packet_size - header_size))
+udp_header_id = bytearray(count*2)
+
+packet_buff = memoryview(udp_packet)
+load_buff = memoryview(udp_payload)
+header_buff = memoryview(udp_header_id)
+
+pi1 = 0
+pi2 = payload_size
+
+hi1 = 0
+hi2 = 2
 
 count_down = count
-while count_down:
 
-    sock.recvfrom_into(mem_buff, packet_size)
+while count_down:
+    sock.recvfrom_into(packet_buff, packet_size)
+    load_buff[pi1:pi2] = packet_buff[header_size:packet_size]
+    header_buff[hi1:hi2] = packet_buff[4:6]
+    packet_buff = packet_buff[packet_size:]
+
+    pi1 += payload_size
+    pi2 += payload_size
+    hi1 += header_id_size
+    hi2 += header_id_size
     count_down -= 1
-    mem_buff = mem_buff[packet_size:]
 
 sock.close()
 
@@ -58,57 +74,32 @@ start_time = time.time()
 udp_payload = np.zeros((packet_size - header_size, count), dtype=np.int16)
 udp_notcont = np.zeros(count, np.bool)
 
-token0 = int.from_bytes(udp_p[4:6], 'big',signed=False)
-token1 = int.from_bytes(udp_p[packet_size+4:packet_size + 6], 'big',
-        signed=False)
+id_arr = np.int32(np.frombuffer(udp_header_id,dtype='>u2'))
 
-cycle = 49999
-# maxid = 0
+id_offsets = np.diff(id_arr) % cycle
 
+no_lost = False
 
-if id_offset(token0, token1, cycle):
-    same_id_before = 0
-else:
-    same_id_before = 1
+if (np.sum(id_offsets) - count // 2) < 2:
+    if np.sum(np.diff(id_offsets)) == 0:
+        no_lost = True
 
-for i in range(count):
-    block_i = i*packet_size
-    h1 = block_i + 4
-    h2 = block_i + 6
-    token1 = int.from_bytes(udp_p[h1:h2], 'big', signed=False)
-
-    diff = id_offset(token0, token1, cycle)
-
-    if same_id_before:
-        if diff > 1:
-            udp_notcont[i] = True
-            print("warning: packet lose", i, token0, token1)
-
-        same_id_before = 0
-    else:
-        if diff > 0:
-            udp_notcont[i] = True
-            print("warning: packet lose", i, token0, token1)
-
-        same_id_before = 1
-
-    token0 = token1
-
-    p1 = block_i + 28
-    p2 = block_i + packet_size
-    udp_payload[:, i] = mem_buff[p1:p2]
+udp_payload = np.frombuffer(load_buff, dtype='>i2')
     
 print("--- %s seconds ---" % (time.time() - start_time))
 sample_rate = 480e6
 one_sample_size = 2 
 size_of_data_per_sec = sample_rate * 2 
-acq_data_size = count * pyload_size 
+acq_data_size = count * payload_size 
 duration  = acq_data_size / size_of_data_per_sec * 1.0
 
 num_lost_p = np.count_nonzero(udp_notcont)
 
-print(f'you have aquired {duration:.3f} \
-        sec of data: {acq_data_size/1024/1204:.3f}  MB')
-
+if no_lost:
+    print(f'you have aquired {duration:.3f} \
+            sec of data: {acq_data_size/1024/1204:.3f}  MB')
+idx = id_offsets > 1
+num_lost_p = len(id_offsets[idx])
 print(num_lost_p, " packet lost", num_lost_p/count * 100, " % lost ")
 
+print("--- %s seconds ---" % (time.time() - start_time))
