14be89da9c558ea715177e524b1846f547309cb1
Quan Guo <guotsuan@gmail.com>
2021-08-24 00:44:22 +0800

    Although put IO to thread, Python Socket is still slow...

--- test.py
+++ test.py
@@ -11,11 +11,12 @@ test DAC sampling
 """
 
 import socket
-import numpy as np
 import sys
 import time
-import h5py as h5
+import threading
 
+# import h5py as h5
+import numpy as np
 
 src_udp_ip = "192.168.90.20"
 src_udp_port = 59000
@@ -25,15 +26,17 @@ udp_port = 60000
 
 cycle = 49999
 
+# sock = socket.socket(socket.AF_INET,  socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 sock = socket.socket(socket.AF_INET,  socket.SOCK_RAW, socket.IPPROTO_UDP)
-err = sock.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, 1073741824)
+err = sock.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, 1610612736)
+sock.setblocking(True)
 # err = sock.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, 1024)
 if err:
     raise ValueError("set socket error")
 
 sock.bind((udp_ip, udp_port))
 
-count = 10000
+count = 2000
 
 payload_size = 8192
 header_size = 28
@@ -49,11 +52,17 @@ load_buff = memoryview(udp_payload)
 header_buff = memoryview(udp_header_id)
 
 
-s_time = time.time()
+num_lost_all = 0.0
+
+def dumpdata(file_name, data, header=None):
+    np.save('/dev/shm/' + file_name, data)
+    # ff = h5.File(file_name, 'w')
+    # ff.create_dataset('voltage', data=data )
+    # ff.close()
 
 packet_buff_head = packet_buff
 
-file_count = 10
+file_count = 2**16
 
 id_head_before = 0
 id_tail_before = 0
@@ -61,6 +70,10 @@ id_tail_before = 0
 # Drop the fist data frame to avoid unstable
 sock.recv(packet_size)
 i = 0
+
+s_time = time.perf_counter()
+time_before = s_time
+
 while True:
 
     pi1 = 0
@@ -72,7 +85,8 @@ while True:
     count_down = count
     packet_buff = packet_buff_head
     while count_down:
-        sock.recvfrom_into(packet_buff, packet_size)
+        sock.recv_into(packet_buff, packet_size)
+
         load_buff[pi1:pi2] = packet_buff[header_size:packet_size]
         header_buff[hi1:hi2] = packet_buff[4:6]
         packet_buff = packet_buff[packet_size:]
@@ -81,9 +95,8 @@ while True:
         pi2 += payload_size
         hi1 += header_id_size
         hi2 += header_id_size
-        count_down -= 1
 
-    start_time = time.time()
+        count_down -= 1
 
     id_arr = np.int32(np.frombuffer(udp_header_id,dtype='>u2'))
     if i > 0:
@@ -112,30 +125,35 @@ while True:
     duration  = acq_data_size / size_of_data_per_sec * 1.0
 
 
-    if no_lost:
-        print(f'you have aquired {duration:.3f} sec, ' +
-                f'{acq_data_size/1024/1204:.3f} MB of data')
-    else:
-        print("program last ", time.time() - s_time)
-        raise ValueError("sampleing is not continues")
+    # if not no_lost:
+        # print("program last ", time.time() - s_time)
+        # raise ValueError("sampleing is not continues")
 
 
     idx = id_offsets > 1
     num_lost_p = len(id_offsets[idx])
-    print(f"{num_lost_p} packet lost, {num_lost_p/count * 100}% of packets lost.")
+    num_lost_all += num_lost_p
+    # print(f"{num_lost_p} packet lost, {num_lost_p/count * 100}% of packets lost.")
 
-    
     # print("--- %s seconds ---" % (time.time() - start_time))
     
-    # fout = 'out_' + str(i)
-    # fout=h5.File(fout, 'w')
-    # fout.create_dataset('voltage', data=udp_payload_arr)
-    # fout.close()
+    
+    k = i % 4
+    if i % 1000 < 4:
+        fout = 'out_' + str(k)
+        writefile = threading.Thread(target=dumpdata, args=(fout,udp_payload_arr))
+        writefile.start()
+    # # fout=h5.File(fout, 'w')
+    # # fout.create_dataset('voltage', data=udp_payload_arr)
+    # # fout.close()
 
-    # time.sleep(0.08)
+    # # time.sleep(0.08)
 
-    if i == 0:
-        i += 1
-    # print("--- %s seconds ---" % (time.time() - start_time))
+    i += 1
+    time_now = time.perf_counter()
+    print("---  seconds ---",time_now - time_before, "----lost_packet:", \
+            num_lost_all, num_lost_all/i/8192)
+    time_before = time_now
+
 
 sock.close()
