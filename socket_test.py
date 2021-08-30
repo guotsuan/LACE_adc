25b9f1e56576a44123ae9c1a5157320f08496cba
Quan Guo <guotsuan@gmail.com>
2021-08-30 19:57:40 +0800

    Update the FPGA code putting SeqNo in payload

---
+++ socket_test.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2021 gq <gq@gqhp>
+#
+# Distributed under terms of the MIT license.
+
+"""
+
+"""
+import socket
+
+src_udp_ip = "192.168.90.20"
+src_udp_port = 59000
+
+udp_ip = "192.168.90.100"
+udp_port = 60000
+
+cycle = 49999
+
+sock = socket.socket(socket.AF_INET,  socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+sock.bind((udp_ip, udp_port))
+
+data = sock.recv(8192)
+
