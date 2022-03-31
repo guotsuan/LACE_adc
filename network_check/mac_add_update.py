fbdc5ab7423a3dd04713b1cb275a6147a5b20e7e
Quan Guo <guoquan@shao.ac.cn>
2022-04-01 05:42:02 +0800

    update mac_add_update and network_check

--- network_check/mac_add_update.py
+++ network_check/mac_add_update.py
@@ -17,19 +17,31 @@ sys.path.append('../')
 from python_udp_recv.params import dst_mac
 
 # mac for receiving output1 and output2
-new_rec_mac = [dst_mac[0], dst_mac[2]]
+new_rec_mac = dst_mac
 
 ssh_cmd = "sshpass -p root ssh  -oHostKeyAlgorithms\=\+ssh-rsa root\@192.168.1.188"
 
+def restart_recv():
+    print("Restat Receiver, pleaset wait for a few secs.......")
+    cmd = ssh_cmd + " \" python zrf8_init.py\""
+    errors= os.system(cmd)
+    if errors:
+        print("Restart Receiver failed....")
+        print("Errors: ", errors)
+    else:
+        print("Restart Restart sucessfully....")
+        
 
-def update_mac(new_mac, pn):
-# pn is 0 or 1
+def update_mac_addr(new_mac, pn):
+    # pn is 0 , 1, 2, 3
 
     i = pn
+
     mac_sendin = "0x" + new_mac.replace(":", "")[4:]
-    cmd = ssh_cmd +  " \"sed -i '/\\s port" + str(i) \
+    cmd = ssh_cmd + " \"sed -i '/\\s port" + str(i) \
         + "_dst_mac0/c\\    port" + str(i) + "_dst_mac0=" + mac_sendin \
                 + "' py2drv.py\""
+
 
     errors= os.system(cmd)
     if errors:
@@ -41,11 +53,51 @@ def update_mac(new_mac, pn):
         print(new_rec_mac[0] + " part1: " + mac_sendin +
                 " was sucessfully Updated\n")
 
+    # update dest_mac1, mac part1 
+    mac_sendin = "0x" + new_mac.replace(":", "")[0:4]
+    cmd = ssh_cmd + " \"sed -i '/\\s port" + str(i) \
+        + "_dst_mac1/c\\    port" + str(i) + "_dst_mac1=" + mac_sendin \
+                + "' py2drv.py\""
+    errors= os.system(cmd)
+
+    if errors:
+        print("Updating Mac Address part2 : " + new_rec_mac[0] +  " part1: " +
+               mac_sendin +  " failed\n")
+        print("Errors: ", errors)
+        return False
+    else:
+        print(new_rec_mac[0] + " part2: " + mac_sendin +
+                " was sucessfully Updated\n")
+
+        return True
+
+def update_dst_ip(new_dst_ip, pn):
+    # pn is 0 , 1, 2, 3
+
+    i = pn
+    dst_ip_sendin = '0x'
+    for x in new_dst_ip.split("."):
+        dst_ip_sendin += hex(int(x))[2:].upper()
+
+    cmd = ssh_cmd +  " \"sed -i '/\\s port" + str(i) \
+        + "_dst_ip/c\\    port" + str(i) + "_dst_ip=" + dst_ip_sendin \
+                + "' py2drv.py\""
+
+    errors= os.system(cmd)
+    if errors:
+        print("Updating Destination IP Address: " + new_dst_ip + " failed\n")
+        print("Errors: ", errors)
+        return False
+    else:
+        print("New destination ip: " + new_dst_ip + " :" + dst_ip_sendin +
+                " was sucessfully Updated\n")
+        return True
+
 def update_all_macs():
     update_ok = True
     for i, new_mac in enumerate(new_rec_mac):
 
-        # update dest_mac0
+        # update dest_mac0, mac port0
         mac_sendin = "0x" + new_mac.replace(":", "")[4:]
         cmd = ssh_cmd + " \"sed -i '/\\s port" + str(i) \
             + "_dst_mac0/c\\    port" + str(i) + "_dst_mac0=" + mac_sendin \
@@ -62,7 +114,7 @@ def update_all_macs():
             print(new_rec_mac[0] + " part1: " + mac_sendin +
                     " was sucessfully Updated\n")
 
-        # update dest_mac0
+        # update dest_mac1, mac part1 
         mac_sendin = "0x" + new_mac.replace(":", "")[0:4]
         cmd = ssh_cmd + " \"sed -i '/\\s port" + str(i) \
             + "_dst_mac1/c\\    port" + str(i) + "_dst_mac1=" + mac_sendin \
@@ -92,4 +144,6 @@ def update_all_macs():
         print("Update failed....please check and try again. Exited with failure...")
 
 if __name__ == "__main__":
+    print("How can I help you")
     update_all_macs()
+    #update_dst_ip("192.168.90.100", 0)
--- network_check/network_check.py
+++ network_check/network_check.py
@@ -17,6 +17,7 @@ from python_udp_recv.params import *
 
 import platform as pf
 import netifaces as nics
+import numpy as np
 
 ########################
 #  General parameters  #
@@ -30,10 +31,12 @@ timeout = 1
 
 # import from params.py
 
-need_to_update_mac = False
+need_to_update_mac = [False] * 4
+need_to_update_dst_ips = [False] * 4
+
+seqs = range(4)
 
 def check_output():
-    need_to_update_mac = False
     print("Current Platform: ", pf.platform(), " Node name: ", pf.node())
     print("Will checking: ", network_faces, "\n")
 
@@ -41,75 +44,95 @@ def check_output():
     nic_of_system = nics.interfaces()
 
     for nic in network_faces:
-        if nic not in nic_of_system:
+        if nic not in nic_of_system and nic != '':
             print("network face: " + nic + "is not in the system, please check again...")
             print("exited....")
             sys.exit()
 
-    num_id = [0,0,1,1] 
 
-    for sip, sport, dip, dport, nic, dmac, lb, nid in zip(src_ip, src_port, dst_ip,
-            dst_port, network_faces, dst_mac, labels, num_id):
+    for i,sip, sport, dip, dport, nic, dmac, lb in zip(seqs, src_ip, src_port, dst_ip,
+            dst_port, network_faces, dst_mac, labels):
 
         print("Checking: ", bcolors.UNDERLINE + lb + bcolors.ENDC)
-        if 'Darwin' in platform_system:
-            # filter option does not working in mac M1
-            p = sniff(count=1, iface=nic, timeout=timeout)
-        else:
-            p = sniff(count=1, iface=nic, filter="udp and host " + sip ,
-                    timeout=timeout)
-        if p:
-            p.nsummary()
-            ok = True
-            p_dst_ip = p[0].sprintf("%-15s,IP.dst%")
-            p_src_ip = p[0].sprintf("%-15s,IP.src%")
-            p_dst_mac = p[0].sprintf("%-18s,Ether.dst%")
-            p_proto = p[0].sprintf("%-8s,IP.proto%")
-
-            if p_dst_ip.strip() != dip.strip():
-                print("destination IP in the packet is different with the parameter")
-                ok=False
-
-            if p_src_ip.strip() != sip.strip():
-                print("source IP in the packet is different with the parameter")
-                print("In packet: ", p_src_ip, "In file: ", sip)
-                ok=False
-
-            if p_dst_mac.strip() != dmac.strip():
-                print("destination MAC address in the packet is different with the parameter")
-                print("Run mac_addr_update.py for you to update the mac registered in the Receiver")
-                ok=False
-                need_to_update_mac = True
-
-            if p_proto.strip() != 'udp':
-                print("Protcol in the packet is not udp")
-                print("In packet: ", p_proto)
-                ok=False
-
-            if ok:
-                print (lb + " seems " + bcolors.OKGREEN + "ok....." + bcolors.ENDC)
-            else:
-                print (lb + bcolors.FAIL + " seems failed....." + bcolors.ENDC)
-
+        if nic == '':
+            print("Ignore the " + lb  + bcolors.OKGREEN + " ok....." + bcolors.ENDC)
             print("\n")
-
         else:
-            print("Connection to the RAW Output1 is broken....\n")
+            if 'Darwin' in platform_system:
+                # filter option does not working in mac M1
+                p = sniff(count=1, iface=nic, timeout=timeout)
+            else:
+                p = sniff(count=1, iface=nic, filter="udp ",
+                        timeout=timeout)
+            if p:
+                p.nsummary()
+                ok = True
+                p_dst_ip = p[0].sprintf("%-15s,IP.dst%")
+                p_src_ip = p[0].sprintf("%-15s,IP.src%")
+                p_dst_mac = p[0].sprintf("%-18s,Ether.dst%")
+                p_proto = p[0].sprintf("%-8s,IP.proto%")
 
-    return need_to_update_mac
+                if p_dst_ip.strip() != dip.strip():
+                    print("destination IP in the packet is different with the parameter ")
+                    print("In packet: ", p_dst_ip, "In file: ", dip +"\n")
+                    ok=False
+                    need_to_update_dst_ips[i] = True
 
+                if p_src_ip.strip() != sip.strip():
+                    print("source IP in the packet is different with the parameter")
+                    print("In packet: ", p_src_ip, "In file: ", sip +"\n")
+                    ok=False
+
+                if p_dst_mac.strip() != dmac.strip():
+                    print("destination MAC address in the packet is different with the parameter")
+                    print("Run mac_addr_update.py for you to update the mac registered in the Receiver")
+                    ok=False
+                    need_to_update_mac[i] = True
+
+                if p_proto.strip() != 'udp':
+                    print("Protcol in the packet is not udp")
+                    print("In packet: ", p_proto)
+                    ok=False
+
+                if ok:
+                    print (lb + " seems " + bcolors.OKGREEN + "ok....." + bcolors.ENDC)
+                else:
+                    print (lb + bcolors.FAIL + " seems failed....." + bcolors.ENDC)
+
+                print("\n")
+
+            else:
+                print("Connection to the RAW Output1 is broken....\n")
+
+    return need_to_update_mac, need_to_update_dst_ips
+
+def check_and_update():
+    need_to_update_mac, need_to_update_dst_ips=check_output()
+
+    seqs = range(4)
+    status = []
+
+    for i, m, p, dip, dmac in zip(seqs, need_to_update_mac, need_to_update_dst_ips,
+            dst_ip, dst_mac):
+        if m:
+            # print("udpate mac port", i)
+            status.append(update_mac_addr(dmac, i))
+        if p:
+            print("udpate dst_ip", i)
+            status.append(update_dst_ip(dip, i))
+
+    if np.all(status):
+        print("restart and check again!")
+        restart_recv()
+        check_output()
+    else:
+        print("mac or IP address update failed... Receiver will not reboot")
 
 if __name__ == "__main__":
-    from mac_add_update import update_all_macs
-
-    need_to_update_mac=check_output()
-    if need_to_update_mac:
-        print(need_to_update_mac)
-        update_all_macs()
-        need_to_update_mac = False
-        check_output()
+    from mac_add_update import update_mac_addr, update_dst_ip, restart_recv
+    check_and_update()
 
 else:
 
-    from network_check.mac_add_update import update_all_macs
+    from network_check.mac_add_update import update_mac_add, update_dst_ip, restart_recv
 
