0fd49520f4aba9c85565d68588be97a49b0ae64e
Quan Guo <guoquan@shao.ac.cn>
2022-04-09 18:26:53 +0800

    fft_rx_test finally work

--- python_udp_recv/rx_helper.py
+++ python_udp_recv/rx_helper.py
@@ -356,27 +356,70 @@ def dumpdata_savez(file_name, data, id_data, block_time):
 
     return
 
-def dumpdata_hdf5(file_name, data, id_data, block_time, fout_dest, file_q):
+def dumpdata_hdf5(file_name, data, id_data, block_time):
 
-        # print("save raw pid: ", os.getpid())
+    # print("save raw pid: ", os.getpid())
 
-        # n_frames_per_loop = data_conf['n_frames_per_loop']
-        # data_size = data_conf['data_size']
-        # n_blocks_to_save  = data_conf['n_blocks_to_save']
-        quantity = 'power'
-        # output_sel = data_conf['output_sel']
-        # file_stop_num = data_conf['file_stop_num']
+    # n_frames_per_loop = data_conf['n_frames_per_loop']
+    # data_size = data_conf['data_size']
+    # n_blocks_to_save  = data_conf['n_blocks_to_save']
+    quantity = 'power'
+    # output_sel = data_conf['output_sel']
+    # file_stop_num = data_conf['file_stop_num']
 
-        f=h5.File(file_name +'.h5','w', driver="core")
-        dset = f.create_dataset(quantity, data=data)
-        dset = f.create_dataset('block_time', data=block_time)
-        # dset.attrs['block_time'] = epoctime2date(block_time)
-        dset = f.create_dataset('block_ids', data=id_data)
+    f=h5.File(file_name +'.h5','w')
+    # f=h5.File(file_name +'.h5','w', driver="core")
+    dset = f.create_dataset(quantity, data=data)
+    dset = f.create_dataset('block_time', data=block_time)
+    # dset.attrs['block_time'] = epoctime2date(block_time)
+    dset = f.create_dataset('block_ids', data=id_data)
 
-        f.close()
+    f.close()
 
-        file_q.put((file_name +'.h5', fout_dest + '.h5'))
-        return
+    return
+
+# def dumpdata_hdf5(file_name, data, id_data, block_time, fout_dest, file_q):
+
+        # # print("save raw pid: ", os.getpid())
+
+        # # n_frames_per_loop = data_conf['n_frames_per_loop']
+        # # data_size = data_conf['data_size']
+        # # n_blocks_to_save  = data_conf['n_blocks_to_save']
+        # quantity = 'power'
+        # # output_sel = data_conf['output_sel']
+        # # file_stop_num = data_conf['file_stop_num']
+
+        # f=h5.File(file_name +'.h5','w', driver="core")
+        # dset = f.create_dataset(quantity, data=data)
+        # dset = f.create_dataset('block_time', data=block_time)
+        # # dset.attrs['block_time'] = epoctime2date(block_time)
+        # dset = f.create_dataset('block_ids', data=id_data)
+
+        # f.close()
+
+        # file_q.put((file_name +'.h5', fout_dest + '.h5'))
+        # return
+# def dumpdata_hdf5(file_name, data, id_data, block_time, fout_dest, file_q):
+
+        # # print("save raw pid: ", os.getpid())
+
+        # # n_frames_per_loop = data_conf['n_frames_per_loop']
+        # # data_size = data_conf['data_size']
+        # # n_blocks_to_save  = data_conf['n_blocks_to_save']
+        # quantity = 'power'
+        # # output_sel = data_conf['output_sel']
+        # # file_stop_num = data_conf['file_stop_num']
+
+        # f=h5.File(file_name +'.h5','w', driver="core")
+        # dset = f.create_dataset(quantity, data=data)
+        # dset = f.create_dataset('block_time', data=block_time)
+        # # dset.attrs['block_time'] = epoctime2date(block_time)
+        # dset = f.create_dataset('block_ids', data=id_data)
+
+        # f.close()
+
+        # file_q.put((file_name +'.h5', fout_dest + '.h5'))
+        # return
 
 def dumpdata_fft_hdf5(file_name, data, id_data, block_time):
 
