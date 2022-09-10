4709ec8654c01b6ba276b428846770ad65e6f42c
Quan Guo <guoquan@shao.ac.cn>
2022-09-11 01:11:23 +0800

    Clean up the folder

---
+++ __init__.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2022 gq <gq@lacebian1>
+#
+# Distributed under terms of the MIT license.
+
+"""
+
+"""
+__package__ = "adc"
+__all__ = ['network_check', 'gps_and_oscillator']
